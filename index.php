3c4ae846810492f2e079f41e4d3db905f219ba4d
Rmei1221 <sshes1221@gmail.com>
2016-04-14 12:20:32 +0800

    b

--- index.php
+++ index.php
@@ -1,35 +1,67 @@
-<?php 
-session_start();
-$counter=1;  
-
-if( !isset($_COOKIE['myCounter']) ){
-  setcookie("myCounter", $counter, time()+30*24*3600 );
-}
-else{
-   $counter = (int)$_COOKIE['myCounter']; 
-  if ( $_SESSION['setCounter'] == TRUE )   
-    setcookie("myCounter", ++$counter, time()+30*24*3600);  
-}
-
-$_SESSION['setCounter']=FALSE;
-$msg = "";
-session_start(); 
-if ($_SESSION["login_session"] != true)
-   header("Location: log.php");  
-else
-   $msg = "歡迎使用者". $_SESSION["username"] ."回來!<br/>";
-?>
-<!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8" />
-<title>網站首頁</title>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <title>計算機</title>
+  <style>
+    body{text-align:center}
+    form{font-size:large;width:240}
+  </style>
 </head>
 <body>
-<?php
-echo "使用者歡迎回來，你上次登入的時間為".$result[0]. ""，你登入的次數為"";
-?>
+  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <input type="text" name="num1" size="3"/> +
+    <input type="text" name="num2" size="3"/> =？
+    <br />
+    <input type="text" name="num3" size="3"/> -
+    <input type="text" name="num4" size="3"/> =？
+    <br />
+    <input type="text" name="num5" size="3"/> *
+    <input type="text" name="num6" size="3"/> =？
+    <br />
+    <input type="text" name="num7" size="3"/> /
+    <input type="text" name="num8" size="3"/> =？
+    <br />
+    <input type="submit" value="輸入完畢, 按我"/>
+    <input type="reset"  value="全部清除"/>
+  </form>
 
-<?php echo $msg; ?>
+<?php
+
+  function add(){
+  //計算加法
+  if ( $_POST['num1'] && $_POST['num2'] ) {
+     echo "{$_POST['num1']} + {$_POST['num2']} = ";
+     echo ($_POST['num1'] + $_POST['num2']);
+     echo '<br />';
+  }
+}
+  function less(){
+  //計算減法
+  if ( $_POST['num3'] && $_POST['num4'] ) {
+     echo "{$_POST['num3']} - {$_POST['num4']} = ";
+     echo ($_POST['num3'] - $_POST['num4']);
+  }
+}
+  function multiply(){
+  //計算乘法
+  if ( $_POST['num5'] && $_POST['num6'] ) {
+     echo "{$_POST['num5']} * {$_POST['num6']} = ";
+     echo ($_POST['num5'] * $_POST['num6']);
+  }
+}
+  function except(){
+  //計算除法
+  if ( $_POST['num7'] && $_POST['num8'] ) {
+     echo "{$_POST['num7']} / {$_POST['num8']} = ";
+     echo ($_POST['num7'] / $_POST['num8']);
+  }
+}
+  function b3(){
+
+
+    
+  }
+
+?>
 </body>
 </html>
